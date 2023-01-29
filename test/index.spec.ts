ed155da758befac72bde78cf73d4b8f89521a12f
Dev XO <code.devxo@gmail.com>
2023-01-29 10:20:12 +0100

    chore: move protocol test inside Basics describe category for consistency

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -83,28 +83,28 @@ describe('OTP Strategy', () => {
         new AuthorizationError('Missing required secret option.'),
       )
     })
-  })
 
-  test('Should properly use `http` protocol for local environments', async () => {
-    const request = new Request(`${HOST_URL}`)
-    const samples: Array<[string, 'http:' | 'https:']> = [
-      ['127.0.0.1', 'http:'],
-      ['127.1.1.1', 'http:'],
-      ['127.0.0.1:8888', 'http:'],
-      ['localhost', 'http:'],
-      ['localhost:3000', 'http:'],
-      ['remix.run', 'https:'],
-      ['remix.run:3000', 'https:'],
-      ['local.com', 'https:'],
-      ['legit.local.com:3000', 'https:'],
-      ['remix-auth-otp.local', 'http:'],
-      ['remix-auth-otp.local:3000', 'http:'],
-    ]
+    test('Should properly use `http` protocol for local environments.', async () => {
+      const request = new Request(`${HOST_URL}`)
+      const samples: Array<[string, 'http:' | 'https:']> = [
+        ['127.0.0.1', 'http:'],
+        ['127.1.1.1', 'http:'],
+        ['127.0.0.1:8888', 'http:'],
+        ['localhost', 'http:'],
+        ['localhost:3000', 'http:'],
+        ['remix.run', 'https:'],
+        ['remix.run:3000', 'https:'],
+        ['local.com', 'https:'],
+        ['legit.local.com:3000', 'https:'],
+        ['remix-auth-otp.local', 'http:'],
+        ['remix-auth-otp.local:3000', 'http:'],
+      ]
 
-    for (const [host, protocol] of samples) {
-      request.headers.set('host', host)
-      expect(getBaseUrl(request).startsWith(protocol)).toBe(true)
-    }
+      for (const [host, protocol] of samples) {
+        request.headers.set('host', host)
+        expect(getBaseUrl(request).startsWith(protocol)).toBe(true)
+      }
+    })
   })
 
   describe('[ 1st Authentication Phase ]', () => {
