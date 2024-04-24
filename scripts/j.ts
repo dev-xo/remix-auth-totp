81530ec061d26813daa2bc070d8579b377a045cb
Michael Wu <mw10013@gmail.com>
2024-04-23 21:23:23 -0400

    hex64

--- scripts/j.ts
+++ scripts/j.ts
@@ -1,8 +1,24 @@
 import * as jose from 'jose'
 
+function hexStringToUint8Array(hexString) {
+    return new Uint8Array(
+      hexString.match(/.{1,2}/g).map(byte => parseInt(byte, 16))
+    );
+  }
+  
+
 // https://github.com/panva/jose/blob/main/docs/functions/key_generate_secret.generateSecret.md
-const secret = await jose.generateSecret('HS256') // 256 bit (32 byte) secret
-console.log('secret: %o', secret)
+// const secret = await jose.generateSecret('HS256') // 256 bit (32 byte) secret
+// const secret = await jose.generateSecret('HS256', { extractable: true }) // 256 bit (32 byte) secret
+// const secret = await jose.generateSecret('A256GCM', { extractable: true }) // 256 bit (32 byte) secret
+// console.log('secret: %o', secret)
+// console.log('secret.toString():', secret.toString())
+// console.log('secret.symmetricKeySize():', secret.symmetricKeySize) // KeyObject in node
+
+// https://www.grc.com/passwords.htm
+// 64 hex characters  =  256 binary bits
+const hex64 = "8089BECBD6DB20F96EE78A6EE81B7076E1A319D5AC7DE27E476E81BF7BF545D4"
+const secret = hexStringToUint8Array(hex64);
 
 // https://github.com/panva/jose/blob/main/docs/classes/jwe_compact_encrypt.CompactEncrypt.md
 const jwe = await new jose.CompactEncrypt(
