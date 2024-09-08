854ef49bd10e081ec18e396d11bd9d0740ebcbc5
f <>
2024-09-08 02:04:47 +0200

    fix formatting and type errors

--- src/utils.ts
+++ src/utils.ts
@@ -26,49 +26,49 @@ export function generateMagicLink(options: {
 
 // https://github.com/sindresorhus/uint8array-extras/blob/main/index.js#L222
 const hexToDecimalLookupTable = {
-	0: 0,
-	1: 1,
-	2: 2,
-	3: 3,
-	4: 4,
-	5: 5,
-	6: 6,
-	7: 7,
-	8: 8,
-	9: 9,
-	a: 10,
-	b: 11,
-	c: 12,
-	d: 13,
-	e: 14,
-	f: 15,
-	A: 10,
-	B: 11,
-	C: 12,
-	D: 13,
-	E: 14,
-	F: 15,
+  0: 0,
+  1: 1,
+  2: 2,
+  3: 3,
+  4: 4,
+  5: 5,
+  6: 6,
+  7: 7,
+  8: 8,
+  9: 9,
+  a: 10,
+  b: 11,
+  c: 12,
+  d: 13,
+  e: 14,
+  f: 15,
+  A: 10,
+  B: 11,
+  C: 12,
+  D: 13,
+  E: 14,
+  F: 15,
 };
 function hexToUint8Array(hexString: string) {
-	if (hexString.length % 2 !== 0) {
-		throw new Error('Invalid Hex string length.');
-	}
+  if (hexString.length % 2 !== 0) {
+    throw new Error('Invalid Hex string length.');
+  }
 
-	const resultLength = hexString.length / 2;
-	const bytes = new Uint8Array(resultLength);
+  const resultLength = hexString.length / 2;
+  const bytes = new Uint8Array(resultLength);
 
-	for (let index = 0; index < resultLength; index++) {
-		const highNibble = hexToDecimalLookupTable[hexString[index * 2]];
-		const lowNibble = hexToDecimalLookupTable[hexString[(index * 2) + 1]];
+  for (let index = 0; index < resultLength; index++) {
+    const highNibble = hexToDecimalLookupTable[hexString[index * 2] as keyof typeof hexToDecimalLookupTable];
+    const lowNibble = hexToDecimalLookupTable[hexString[(index * 2) + 1] as keyof typeof hexToDecimalLookupTable];
 
-		if (highNibble === undefined || lowNibble === undefined) {
-			throw new Error(`Invalid Hex character encountered at position ${index * 2}`);
-		}
+    if (highNibble === undefined || lowNibble === undefined) {
+      throw new Error(`Invalid Hex character encountered at position ${index * 2}`);
+    }
 
-		bytes[index] = (highNibble << 4) | lowNibble;
-	}
+    bytes[index] = (highNibble << 4) | lowNibble;
+  }
 
-	return bytes;
+  return bytes;
 }
 
 /**
